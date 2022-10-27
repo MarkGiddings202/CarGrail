0db1798c4a62061dc02216421cfed976ba21e952
Delvin Reyes <DelvinReyes.95@Gmail.com>
2022-10-27 13:27:18 -0400

    modification to model files

---
+++ models/financeModel.js
@@ -0,0 +1,25 @@
+const { pool } = require('../db.js')
+
+class Finance {
+    static async getAllfinance(id){
+        const database = 'SELECT * FROM finance WHERE user_id = $1'
+        const dbResults = await pool.query(database, [id])
+        return dbResults.rows
+    }
+
+
+    // finance.getAllfinance().then(result => console.log(result));
+
+// static async createFinance(){
+//         const database = `INSERT INTO finance (expenses, income, savings, user_id, budget) VALUES ($1, $2, $3, $4, $5) RETURNING *`
+//         const dbResults = await pool.query(database, [12000, 10000, 30000, 2, 21000])
+//         return dbResults.rows[0]
+//     }
+
+// Finance.createFinance().then(result => console.log(result));
+
+  
+
+}
+
+module.exports = Finance
