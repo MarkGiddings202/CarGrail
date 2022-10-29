9772237fe5a96f145dd6824e34e5eebfd2aa4084
Gavin Giddings <Gavin>
2022-10-29 18:37:39 -0400

    mg/fix-connection

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  database: "CarGrail",
-  user: "postgres",
-  password: "",
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = {
