98b7c1351c4575aecb3fbcf445cf55818c134010
Delvin Reyes <DelvinReyes.95@Gmail.com>
2022-11-01 00:30:41 -0400

    added 2 more seeds

--- public/src/homePage.js
+++ public/src/homePage.js
@@ -1,6 +1,8 @@
 // When adding more cars to the db, be sure to add it here like this {brandmodel}: imageUrl (Find an image from anywhere)
 const carImageMap = {
   nissanaltima: 'https://mysterio.yahoo.com/mysterio/api/9A79FF07E1AD9A29B3C3E7AF861F7FBF5AA405CD13105CDAF6002E8B72E183FA/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/USD20NIC041A021001.jpg',
+  mazda3: "https://www.mazdausa.com/siteassets/vehicles/2023/m3h/06_btv/ext-360s/2.5/snowflake-white-pearl/e360-my22-m3h-2-5s-snowflake-white-pearl-005.jpg",
+  volkswagentiguan: "https://www.edmunds.com/assets/m/for-sale/b2-3vv3b7ax2km192535/img-1-600x400.jpg",
     fordfusion: 'https://www.autotrader.com/wp-content/uploads/2020/08/WC_2019-Ford-Fusion-Hybrid-Copper-Action-1.jpg',
     hondaaccord: 'https://www.motortrend.com/uploads/2022/02/2023-Honda-Accord-rendering-01.jpg',
     toyotacamry: "https://toyota-cms-media.s3.amazonaws.com/wp-content/uploads/2020/07/MY21_Camry_XSE_Hybrid_001-1500x900.jpg",
